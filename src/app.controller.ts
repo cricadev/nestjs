20b711ac9b70426485f0445b884f8134bf8089cf
CristianN998 <cristiancasallas998@gmail.com>
2023-12-07 14:48:38 -0500

    tour rest api nestjs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,38 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/new-endpoint')
+  newEndpoint() {
+    return 'soy un nuevo endpoiaadfafdsant';
+  }
+
+  @Get('/products/')
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`;
+  }
+
+  @Get('/products/filter')
+  getProductFilter(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`;
+  }
+
+  @Get('/products/:productId')
+  getProduct(@Param('productId') productId: string) {
+    return 'product ' + productId;
+  }
+
+  @Get('/categories/:id/products/:productId')
+  getCategoru(@Param() { id, productId }) {
+    return 'product ' + productId + ' and category ' + id;
   }
 }
---
+++ src/recap.ts
@@ -0,0 +1,24 @@
+const myName = 'Cricadev';
+const myAge = 25;
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+sum(1, myAge);
+
+class Persona {
+
+  age;
+  name;
+
+  constructor(age: number, name: string) { }
+
+  getSummary() {
+    return `my name is ${this.name} and my age is ${this.age}`;
+  }
+}
+
+const cricadev = new Persona(myAge, myName);
+
+console.log(cricadev);
